0c58acaaf50a26f45d49139ff805d1e59ebb99a2
ObidovSamandar <samandarobidovfrd@gmail.com>
2021-06-01 11:36:39 +0500

    updated

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require('express')
 const config = require('./configs/config')
 const app = express()
-// const db = require('./storage/mongo')
-// const http = require('http')
-// const server = http.createServer(app)
-// const { Server } = require('socket.io')
-// const io = new Server(server)
-// const path = require('path')
+const db = require('./storage/mongo')
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+const path = require('path')
 
-// ;(async _=>{
-//     try {
-//         await db()
-//         console.log('Mongodb database connection established')
-//     } catch (e) {
-//         console.log(e.message)
-//     }
-// })()
+;(async _=>{
+    try {
+        await db()
+        console.log('Mongodb database connection established')
+    } catch (e) {
+        console.log(e.message)
+    }
+})()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
-// app.use('/socket', express.static(path.join(__dirname, 'node_modules', "socket.io", "client-dist")))
+app.use('/socket', express.static(path.join(__dirname, 'node_modules', "socket.io", "client-dist")))
 
 app.set('view engine', "ejs")
-app.listen(config.HTTPORT, () => console.log(`SERVER LISTEN AT ${config.HTTPORT}`))
+server.listen(config.HTTPORT, () => console.log(`SERVER LISTEN AT ${config.HTTPORT}`))
 
 
 
@@ -32,21 +32,21 @@ app.get('/',  (req, res) => {
     })
 })
 
-// app.get('/register',  (req,res)=>{
-//     console.log(req.query)
+app.get('/register',  (req,res)=>{
+    console.log(req.query)
 
-//     let { id, first_name, user_name, auth_data, hash } = req.query
+    let { id, first_name, user_name, auth_data, hash } = req.query
 
-//     res.redirect('/chat')
-// })
+    res.redirect('/chat')
+})
 
-// app.get('/chat', (req, res)=>{
-//     res.render('chat',{
-//         title:'Telegram'
-//     })
-// })
+app.get('/chat', (req, res)=>{
+    res.render('chat',{
+        title:'Telegram'
+    })
+})
 
 
-// io.on('connection', async (socket) => {
-//     console.log(socket.id)
-// })
+io.on('connection', async (socket) => {
+    console.log(socket.id)
+})
