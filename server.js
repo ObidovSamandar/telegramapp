f3be66539acb1556640812c27380150dbcbe7fbe
ObidovSamandar <samandarobidovfrd@gmail.com>
2021-06-01 12:37:24 +0500

    created

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const { Server } = require('socket.io')
 const io = new Server(server)
 const path = require('path')
 
+const DATA = []
 ;(async _=>{
     try {
         await db()
@@ -47,6 +48,58 @@ app.get('/chat', (req, res)=>{
 
 
 io.on('connection', (socket) => {
-    console.log(socket.id,'bizga qoshildi')
-})
+    
+    console.log(socket.id, "biz qoshildi");
+    
+    let id = DATA.find(user => user.id == socket.id)
+    if(!id){
+        DATA.push({
+            id: socket.id
+        })
+    }
+    
+    
+    socket.on('sign', data => {
+        const index = DATA.findIndex(user => user.id == socket.id)
+        
+        if(DATA[index]["name"]){
+            socket.emit('error', {
+                message: "Siz ism kiritgansiz"
+            })
+        } else {
+            DATA[index]["name"] = data?.name
+            
+            socket.emit("logged", DATA.filter(e => e.name))
+            
+            socket.broadcast.emit('new_member', {
+                id: socket.id,
+                name: data.name
+            })
+        }
+        
+        // socket.emit('logged', {
+        //     logged: true
+        // })
+    })
+    
+    
+    socket.on('disconnect', () => {
+        let index = DATA.findIndex(user => user.id == socket.id)
+        DATA.splice(index, 1)
+        console.log(socket.id, "bizni tark etdi");
+        
+        socket.broadcast.emit('left_member', {
+            id: socket.id,
+        })        
+    });
 
+    socket.on('new_message', data => {
+        let index = DATA.findIndex(user => user.id == socket.id)
+        if(DATA[index]["name"]){
+            socket.broadcast.emit('new_message', {
+                owner: DATA[index]["name"],
+                message: data.message
+            })
+        }
+    })
+})
