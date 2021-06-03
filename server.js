f3c42bc1823622c18a1df89a3fe7020026b6fe4f
ObidovSamandar <samandarobidovfrd@gmail.com>
2021-06-03 09:26:45 +0500

    updated

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express()
 const db = require('./storage/mongo')
 const http = require('http')
 const server = http.createServer(app)
-// const { Server } = require('socket.io')
-// const io = new Server(server)
-// const path = require('path')
-// const fetch = require('node-fetch')
+const { Server } = require('socket.io')
+const io = new Server(server)
+const path = require('path')
+const fetch = require('node-fetch')
 
 
 const userModel = require('./controller/UserRegisterController')
@@ -26,7 +26,7 @@ const user  = new userModel()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
-// app.use('/socket', express.static(path.join(__dirname, 'node_modules', "socket.io", "client-dist")))
+app.use('/socket', express.static(path.join(__dirname, 'node_modules', "socket.io", "client-dist")))
 
 app.set('view engine', "ejs")
 
@@ -47,21 +47,21 @@ app.get('/',  (req, res) => {
 })
 
 
-// app.get('/register', async  (req,res)=>{
-//     let { id, first_name, user_name, auth_data, hash } = req.query
-//     let createUser = await user.createUser({
-//         name:first_name,
-//         user_name,
-//         chat_id:id,
-//     })
+app.get('/register', async  (req,res)=>{
+    let { id, first_name, user_name, auth_data, hash } = req.query
+    let createUser = await user.createUser({
+        name:first_name,
+        user_name,
+        chat_id:id,
+    })
   
 
-//     let sendMessageTOBot = await fetch(`https://api.telegram.org/bot${config.BOT_TOKEN}/sendMessage?chat_id=${config.Bot_id}&text=\n name:${first_name} \n username:${user_name}`, {
-//         method: 'POST',
-//     })
-//     console.log(sendMessageTOBot)
-//     res.redirect('/chat')
-// })
+    let sendMessageTOBot = await fetch(`https://api.telegram.org/bot${config.BOT_TOKEN}/sendMessage?chat_id=${config.Bot_id}&text=\n name:${first_name} \n username:${user_name}`, {
+        method: 'POST',
+    })
+    console.log(sendMessageTOBot)
+    res.redirect('/chat')
+})
 
 app.get('/chat', (req, res)=>{
     res.render('chat',{
